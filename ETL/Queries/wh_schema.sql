e3e6caa33566ecb7745889b19bb79fbdf0985307
Kartikye <Legionnaires_51@outlook.com>
2022-05-14 12:52:35 -0400

    ETL complete

--- ETL/Queries/wh_schema.sql
+++ ETL/Queries/wh_schema.sql
@@ -22,3 +22,27 @@ Select * FROM gini_index;
 Select * FROM unemployment;
 
 
+
+-- Merge data on "Country" into new table happy_complete
+
+	-- Drop Table if error occurrs(uncomment code below)
+	-- DROP TABLE happy_complete;
+
+SELECT hc.*, g."Index of institutional trust", g."Gini coefficient of income",
+unemp."2021", cc."latitude", cc."longitude"
+INTO happy_complete
+from happiness_clean as hc
+LEFT JOIN gini_index as g on (g."Country" = hc."Country")
+LEFT JOIN unemployment as unemp on (unemp."Country" = hc."Country")
+LEFT JOIN country_coordinates as cc on (cc."Country" = hc."Country");
+
+Select * From happy_complete;
+
+
+-- Create Country Coordinates with Happiness scores
+SELECT cc.*, hc."Happiness score"
+INTO happy_coord
+from country_coordinates as cc
+LEFT JOIN happiness_clean as hc on (hc."Country" = cc."Country");
+
+Select * From happy_coord;
