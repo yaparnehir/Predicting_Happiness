8816e7a4142de3ca6713230bebcedb2022c8df3c
Kartikye <Legionnaires_51@outlook.com>
2022-05-13 21:26:15 -0400

    need to create merged datasets

---
+++ ETL/Queries/wh_schema.sql
@@ -0,0 +1,24 @@
+--View all original cleaned data
+
+-- View World_happiness 2021
+	-- Drop Table if error occurrs(uncomment code below)
+	-- DROP TABLE happiness_clean;
+SELECT * FROM happiness_clean;
+
+
+--View Country Coordinates
+	-- Drop Table if error occurrs(uncomment code below)
+	-- DROP TABLE country_coordinates;
+Select * FROM country_coordinates;
+
+-- View Gini Index
+	-- Drop Table if error occurrs(uncomment code below)
+	-- DROP TABLE gini_index;
+Select * FROM gini_index;
+
+-- View Unemployment
+	-- Drop Table if error occurrs(uncomment code below)
+	-- DROP TABLE unemployment;
+Select * FROM unemployment;
+
+
